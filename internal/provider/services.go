d28c61961e9785c06a3fc7970ff7d6e8ed3404d9
tombuildsstuff <git@tom.ibuildstuff.eu>
2022-04-19 14:21:11 +0200

    dev: adding a fake data source to highlight the feature-toggle works

--- internal/provider/services.go
+++ internal/provider/services.go
@@ -100,6 +100,7 @@ import (
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/streamanalytics"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/subscription"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/synapse"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/services/tombuildsstuff"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/trafficmanager"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/videoanalyzer"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/vmware"
@@ -130,6 +131,7 @@ func SupportedTypedServices() []sdk.TypedServiceRegistration {
 		sentinel.Registration{},
 		servicefabricmanaged.Registration{},
 		streamanalytics.Registration{},
+		tombuildsstuff.Registration{},
 		web.Registration{},
 	}
 }
